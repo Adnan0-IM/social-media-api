a536168de2717ca6b63462999cf645d54283588a
Adnan0-IM <adnanilyas969@gmail.com>
2025-09-04 00:26:37 +0100

    fix mongo-uri

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,7 +4,7 @@ dotenv.config();
 const local = "mongodb://127.0.0.1:27017/social-media";
 export default {
   PORT: process.env.PORT || 3000,
-  MONGO_URI: local || process.env.MONGO_URI ,
+  MONGO_URI: process.env.MONGO_URI || local,
   JWT_SECRET: process.env.JWT_SECRET,
   NODE_ENV: process.env.NODE_ENV,
 };
